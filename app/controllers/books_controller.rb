21e1f311927f152940d6872d06b9e2ac49dbe7a5
toshi004 <d.toshi004.35@gmail.com>
2021-06-05 16:02:31 +0000

    [Add]編集画面の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
@@ -29,7 +35,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.permit(:title,:body)
+    params.require(:book).permit(:title,:body)
   end
 
 end
