981ee3574e09d09031254eb3f6cb031e6591d354
toshi004 <d.toshi004.35@gmail.com>
2021-06-05 15:28:44 +0000

    [Update]indexのhtml追記

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,35 @@
 class BooksController < ApplicationController
+  def new
+    @book = book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.permit(:title,:body)
+  end
+
 end
